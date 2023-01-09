87429530ad2fdae67414384147737e9ca07ff6dc
Алексей Ермаков <lexlaeda@Aleksejs-MacBook-Pro.local>
2023-01-09 23:46:38 +0400

    add add employee

--- src/employees/EmployeePage.tsx
+++ src/employees/EmployeePage.tsx
@@ -1,13 +1,82 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import {useEmployees} from "../hooks/EmployeeHooks";
 import EmployeeTable from "./component/EmployeeTable";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Fab, TextField} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 
 function EmployeePage() {
+    const [open, setOpen] = useState(false);
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [middleName, setMiddleName] = useState('');
     console.log("I am loading Employee page")
     const {employees, addEmployee} = useEmployees();
-    console.log(employees)
-    return <EmployeeTable employees={employees}/>
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleSave = () => {
+        addEmployee({firstname: name, surname: surname, middlename: middleName})
+        handleClose();
+    }
+
+
+    return <>
+        <Dialog open={open} onClose={handleClose} fullWidth>
+            <DialogTitle>Add employee</DialogTitle>
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Name"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="surname"
+                    label="Surname"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setSurname(e.target.value)}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="middlename"
+                    label="Middle name"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setMiddleName(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleSave}>Save</Button>
+            </DialogActions>
+        </Dialog>
+        <EmployeeTable employees={employees}/>
+        <Fab color="primary" aria-label="add" sx={{
+            position: 'fixed',
+            bottom: '25px',
+            right: '25px'
+        }} onClick={() => setOpen(true)}>
+            <AddIcon/>
+        </Fab>
+
+    </>
 }
 
 export default EmployeePage;
--- src/hooks/EmployeeHooks.ts
+++ src/hooks/EmployeeHooks.ts
@@ -2,10 +2,17 @@ import {IEmployee} from "../employees/interface/Employee";
 import {useEffect, useState} from "react";
 import {proxyAxios} from "../backend/Constant";
 
-export interface UseEmployee{
+export interface UseEmployee {
     employees: IEmployee[],
-    addEmployee: (employee: IEmployee) => void
+    addEmployee: (employee: EmployeeCreateRequest) => void
 }
+
+export interface EmployeeCreateRequest {
+    firstname: string;
+    surname: string;
+    middlename: string;
+}
+
 export function useEmployees(): UseEmployee {
     const [employees, setEmployees] = useState<IEmployee[]>([]);
     // const [error, setError] = useState<String>();
@@ -19,8 +26,13 @@ export function useEmployees(): UseEmployee {
             })
     }, [])
 
-    const addEmployee = (employee: IEmployee) => {
-        setEmployees([...employees ,employee])
+    const addEmployee = (employee: EmployeeCreateRequest) => {
+        proxyAxios.post<IEmployee>('/employee', employee)
+            .then(result => {
+                if (result.data !== undefined){
+                    setEmployees([...employees, result.data])
+                }
+            })
     }
     return {
         employees: employees,
