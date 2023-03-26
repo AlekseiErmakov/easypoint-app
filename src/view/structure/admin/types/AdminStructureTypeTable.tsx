f15446fa303b5daad689c08ad6305f4f1ae5a7be
Алексей Ермаков <lexlaeda30@gmail.com>
2023-03-26 18:17:45 +0400

    add eslint

--- src/view/structure/admin/types/AdminStructureTypeTable.tsx
+++ src/view/structure/admin/types/AdminStructureTypeTable.tsx
@@ -1,6 +1,7 @@
 import { type ColumnsType } from 'antd/es/table'
 import { Table } from 'antd'
 import { type IAdminStructureType } from './index'
+import React from 'react'
 
 const columns: ColumnsType<IAdminStructureType> = [
   {
